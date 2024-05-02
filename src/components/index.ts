6c3693d5dc7497ad846c25756bc4fb0fa0943fc2
Mark Louie <dullavin.marklouie.d@gmail.com>
2024-05-02 17:56:21 +0800

    Initial Commit

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Header from "./Header/Header";
+import CardsContaainer from "./CardsContainer/CardsContaainer";
+import Card from "./Card/Card";
+
+export { Header, CardsContaainer, Card };
---
+++ types.d.ts
@@ -0,0 +1,6 @@
+export type TPokemonType = {
+  pokemon_id: number | null;
+  pokemon_avatar: string;
+  pokemon_name: string;
+  pokemon_desc: string;
+};
